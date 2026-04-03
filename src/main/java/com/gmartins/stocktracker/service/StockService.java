50397b126e2c28ec621273cce83bb4e62dbf5a67
Gabriel Martins <gab.smartinn@outlook.com>
2026-04-03 16:21:17 -0300

    feat: implement business logic to add a purchase to an existing stock

--- src/main/java/com/gmartins/stocktracker/service/StockService.java
+++ src/main/java/com/gmartins/stocktracker/service/StockService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,11 +18,20 @@ public class StockService {
     private final StockPurchaseRepository stockPurchaseRepository;
 
     public Stock savePurchase(Stock stock, StockPurchase stockPurchase) {
-
         StockPurchase savedStockPurchase = stockPurchaseRepository.save(stockPurchase);
         stock.setPurchases(List.of(savedStockPurchase));
         return stockRepository.save(stock);
     }
 
+    public void addPurchase(String stockId, StockPurchase stockPurchase) {
+        Optional<Stock> optStock = stockRepository.findById(stockId);
+
+        optStock.map(stock -> {
+            StockPurchase savedStockPurchase = stockPurchaseRepository.save(stockPurchase);
+            stock.getPurchases().add(savedStockPurchase);
+
+            return stockRepository.save(stock);
+        }).orElseThrow(() -> new IllegalArgumentException("Cannot find stock with id: " + stockId));
+    }
 
 }
