13c500884583fdf3f01b575cba4e2cd7675c3cb9
Gabriel Martins <gab.smartinn@outlook.com>
2026-04-03 16:51:24 -0300

    feat: expose endpoint to list all stocks and purchases

--- src/main/java/com/gmartins/stocktracker/controller/StockController.java
+++ src/main/java/com/gmartins/stocktracker/controller/StockController.java
@@ -10,10 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.gmartins.stocktracker.mapper.StockMapper.toStockPurchase;
 
@@ -41,6 +40,11 @@ public class StockController {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.unprocessableEntity().build();
         }
-
     }
+
+    @GetMapping
+    public ResponseEntity<List<Stock>> findAll() {
+        return ResponseEntity.ok(stockService.findAll());
+    }
+
 }
