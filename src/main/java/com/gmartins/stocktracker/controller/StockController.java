dc8abc3ddd3860d2371faff47f3d4acd00c683ff
Gabriel Martins <gab.smartinn@outlook.com>
2026-04-03 16:21:20 -0300

    feat: expose endpoint to register new stock purchases

--- src/main/java/com/gmartins/stocktracker/controller/StockController.java
+++ src/main/java/com/gmartins/stocktracker/controller/StockController.java
@@ -1,5 +1,6 @@
 package com.gmartins.stocktracker.controller;
 
+import com.gmartins.stocktracker.controller.request.StockAddPurchaseRequest;
 import com.gmartins.stocktracker.controller.request.StockRequest;
 import com.gmartins.stocktracker.entity.Stock;
 import com.gmartins.stocktracker.entity.StockPurchase;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.gmartins.stocktracker.mapper.StockMapper.toStockPurchase;
+
 @RestController
 @RequestMapping("/api/v1/stock")
 @RequiredArgsConstructor
@@ -23,10 +26,21 @@ public class StockController {
 
     @PostMapping
     public ResponseEntity<Stock> savePurchase(@RequestBody StockRequest request) {
-
         Pair<Stock, StockPurchase> stock = StockMapper.toStock(request);
         Stock savedStock = stockService.savePurchase(stock.getFirst(), stock.getSecond());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedStock);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Void> addPurchase(@RequestBody StockAddPurchaseRequest request) {
+        try {
+            stockService.addPurchase(request.getStockId(), toStockPurchase(request));
+
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
     }
 }
