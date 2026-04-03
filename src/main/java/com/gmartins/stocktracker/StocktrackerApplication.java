173979f0bf6ab5b1b5f9a6aec728eaae5e7fb850
Gabriel Martins <gab.smartinn@outlook.com>
2026-04-03 16:51:10 -0300

    build: setup feign client dependencies and properties for brapi

--- src/main/java/com/gmartins/stocktracker/StocktrackerApplication.java
+++ src/main/java/com/gmartins/stocktracker/StocktrackerApplication.java
@@ -2,8 +2,10 @@ package com.gmartins.stocktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class StocktrackerApplication {
 
 	public static void main(String[] args) {
