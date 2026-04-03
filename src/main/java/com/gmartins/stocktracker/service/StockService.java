d5a267f937b60ab9e9690778a3eaf036fa5ccf70
Gabriel Martins <gab.smartinn@outlook.com>
2026-04-03 16:51:22 -0300

    feat: implement stock retrieval enriched with real-time market prices

--- src/main/java/com/gmartins/stocktracker/service/StockService.java
+++ src/main/java/com/gmartins/stocktracker/service/StockService.java
@@ -1,12 +1,16 @@
 package com.gmartins.stocktracker.service;
 
+import com.gmartins.stocktracker.client.BrapiClient;
+import com.gmartins.stocktracker.client.response.BrapiStockResponse;
 import com.gmartins.stocktracker.entity.Stock;
 import com.gmartins.stocktracker.entity.StockPurchase;
 import com.gmartins.stocktracker.repository.StockPurchaseRepository;
 import com.gmartins.stocktracker.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,8 +18,12 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class StockService {
 
+    @Value("${stock.client.brapi.token}")
+    private String token;
+
     private final StockRepository stockRepository;
     private final StockPurchaseRepository stockPurchaseRepository;
+    private final BrapiClient brapiClient;
 
     public Stock savePurchase(Stock stock, StockPurchase stockPurchase) {
         StockPurchase savedStockPurchase = stockPurchaseRepository.save(stockPurchase);
@@ -34,4 +42,14 @@ public class StockService {
         }).orElseThrow(() -> new IllegalArgumentException("Cannot find stock with id: " + stockId));
     }
 
+    public List<Stock> findAll() {
+        List<Stock> stocks = stockRepository.findAll();
+
+        stocks.forEach(stock -> {
+            BrapiStockResponse brapiStockResponse = brapiClient.getStock(stock.getStock(), token);
+            stock.setPrice(BigDecimal.valueOf(brapiStockResponse.getResults().get(0).getRegularMarketPrice()));
+        });
+
+        return stocks;
+    }
 }
