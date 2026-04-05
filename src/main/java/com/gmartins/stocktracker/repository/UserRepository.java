5be3226031005978f7ca8dce41efab90b4849ffa
Gabriel Martins <gab.smartinn@outlook.com>
2026-04-04 22:01:46 -0300

    feat: add user repository for database operations

---
+++ src/main/java/com/gmartins/stocktracker/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gmartins.stocktracker.repository;
+
+import com.gmartins.stocktracker.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    UserDetails findByEmail(String email);
+}
